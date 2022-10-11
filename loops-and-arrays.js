73f4b9de5c9c7ef2769f551c882f040c926dc8de
Klippie <info@johanvanderklift.nl>
2022-10-11 16:57:21 +0200

    loops-and-arrays.js finished

--- loops-and-arrays.js
+++ loops-and-arrays.js
@@ -6,6 +6,14 @@
 
 const names = ["Henk", "Piet", "Fred", "Joop"];
 
+console.log(names);
+
+for (let i=0; i<names.length; i++) {
+    names[i] = names[i] + "je";
+}
+
+console.log(names);
+
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
 // console.log(names) geeft: ["Henk", "Piet", "Fred", "Joop"]
@@ -22,6 +30,18 @@ const names = ["Henk", "Piet", "Fred", "Joop"];
 // ==========================================
 
 const numbers = [2, 4, 5, 29, 38];
+
+console.log(numbers)
+
+for (let i=0; i<numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+        numbers[i] = numbers[i] * 2;
+    } else {
+        numbers[i] = numbers[i] * 3;
+    }
+}
+
+console.log(numbers)
 
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
@@ -41,6 +61,14 @@ const numbers = [2, 4, 5, 29, 38];
 // ==========================================
 
 const squares = [30, 2, 8, 24, 11];
+
+console.log(squares);
+
+for (let i=0; i<squares.length; i++) {
+    squares[i] = "Het volume van " + squares[i] + " is " + (squares[i] ** 3);
+}
+
+console.log(squares)
 
 // Verwachtte uitkomsten:
 // Vóór het script zie je de originele waardes:
